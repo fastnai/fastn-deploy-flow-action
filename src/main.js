3c14a2c857752e3e86d15d7a3a8359e485a54f30
saqadir <ammar@fastn.ai>
2024-05-22 01:22:55 +0500

    fixes and updates for prerelease

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core')
-const { wait } = require('./wait')
+const axios = require('axios')
+const qs = require('qs')
 
 /**
  * The main function for the action.
@@ -7,22 +8,195 @@ const { wait } = require('./wait')
  */
 async function run() {
   try {
-    const ms = core.getInput('milliseconds', { required: true })
+    const srcUsername = core.getInput('source-account-username', {
+      required: true
+    })
+    const srcPassword = core.getInput('source-account-password', {
+      required: true
+    })
+    const srcEnv = core.getInput('source-env', {
+      required: true
+    })
+    const srcProjectId = core.getInput('source-project-id', {
+      required: true
+    })
+    const srcFlowNameInput = core.getInput('source-flow-name', {
+      required: true
+    })
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const desUsername = core.getInput('destination-account-username', {
+      required: true
+    })
+    const desPassword = core.getInput('destination-account-password', {
+      required: true
+    })
+    const desEnv = core.getInput('destination-env', {
+      required: true
+    })
+    const desProjectId = core.getInput('destination-project-id', {
+      required: true
+    })
+    const desFlowNameInput = core.getInput('destination-flow-name')
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const srcAuthToken = await fetchAuthToken(
+      srcUsername,
+      srcPassword,
+      pickEnv(srcEnv)
+    )
+    core.debug(`Fetched source auth token: ${srcAuthToken} for ${srcUsername}`)
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    const srcFlowNames = srcFlowNameInput
+      .split(', ')
+      .filter(item => item.trim() !== '')
+    console.log(`Flow names input: ${srcFlowNames}`)
+    const fetchedApis = []
+
+    for (const flowName of srcFlowNames) {
+      const fetchedApi = await fetchApiFlow(
+        srcAuthToken,
+        srcProjectId,
+        flowName,
+        pickEnv(srcEnv)
+      )
+      fetchedApis.push(fetchedApi)
+      core.debug(
+        `Fetched flow: ${JSON.stringify(fetchedApi)} for ${srcUsername}`
+      )
+    }
+
+    const desAuthToken = await fetchAuthToken(
+      desUsername,
+      desPassword,
+      pickEnv(desEnv)
+    )
+    core.debug(`Fetched source auth token: ${desAuthToken} for ${desUsername}`)
+
+    let desFlowNames
+
+    if (desFlowNameInput) {
+      desFlowNames = desFlowNameInput
+        .split(',')
+        .filter(item => item.trim() !== '')
+    } else {
+      desFlowNames = srcFlowNames
+    }
+
+    let nameCount = 0
+    for (const api of fetchedApis) {
+      await importApiFlow(
+        desAuthToken,
+        desProjectId,
+        desFlowNames[nameCount],
+        api,
+        pickEnv(desEnv)
+      )
+
+      core.debug(
+        `Imported ${srcFlowNames[nameCount]} from ${srcProjectId} as ${desFlowNames[nameCount]} in ${desProjectId}`
+      )
+      nameCount++
+    }
   } catch (error) {
-    // Fail the workflow run if an error occurs
     core.setFailed(error.message)
   }
+}
+
+// Fetch token function
+async function fetchAuthToken(username, password, env) {
+  const data = qs.stringify({
+    grant_type: 'password',
+    username,
+    password,
+    client_id: 'fastn-app',
+    scope: 'openid'
+  })
+
+  const config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: `https://auth.${env}/realms/fastn/protocol/openid-connect/token`,
+    headers: {
+      realm: 'fastn',
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    data
+  }
+
+  try {
+    const response = await axios.request(config)
+    return response.data.access_token
+  } catch (error) {
+    core.debug(
+      `Unable to retrieve auth token for ${username} in ${env}. Error: ${error}`
+    )
+  }
+}
+
+// Fetch API
+async function fetchApiFlow(authToken, projectId, apiName, env) {
+  const data = JSON.stringify({
+    query: `query exportApi($input: GetEntityInput) {
+    exportApi(input: $input) 
+  }`,
+    variables: { input: { clientId: projectId, id: apiName } }
+  })
+
+  const config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: `https://api.${env}/graphql`,
+    headers: {
+      authorization: `Bearer ${authToken}`,
+      'content-type': 'application/json',
+      'fastn-space-id': projectId,
+      stage: 'LIVE',
+      realm: 'fastn'
+    },
+    data
+  }
+
+  try {
+    const response = await axios.request(config)
+    return response.data.data.exportApi
+  } catch (error) {
+    core.debug(`Unable to fetch flow ${apiName}. Error: ${error}`)
+  }
+}
+
+// Import API Flow
+async function importApiFlow(authToken, projectId, apiName, apiData, env) {
+  const data = JSON.stringify({
+    query: `mutation importApi($input: ImportApiInput!) {
+      importApi(input: $input) {
+        __typename
+      }
+    }`,
+    variables: { input: { clientId: projectId, name: apiName, data: apiData } }
+  })
+
+  const config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: `https://api.${env}/graphql`,
+    headers: {
+      authorization: `Bearer ${authToken}`,
+      'content-type': 'application/json',
+      'fastn-space-id': projectId,
+      realm: 'fastn'
+    },
+    data
+  }
+
+  try {
+    const response = await axios.request(config)
+  } catch (error) {
+    core.debug(`Unable to import flow ${apiName}. Error: ${error}`)
+  }
+}
+
+//Helper - set url
+function pickEnv(env) {
+  return env
 }
 
 module.exports = {
